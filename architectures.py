2427d3f26b4d3d4c27e8f229139101398d9ae152
Dominik Muehlbacher <muehlbacher.dominik@gmail.com>
2022-08-15 18:47:40 +0200

    Datasets

---
+++ architectures.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+
+class MModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(
+            
+            nn.Conv2d(3, 32, kernel_size = 3, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(32,64, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+        
+            nn.Conv2d(64, 128, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(128 ,128, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            
+            nn.Conv2d(128, 256, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.Conv2d(256,256, kernel_size = 3, stride = 1, padding = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            
+            nn.Flatten(),
+            nn.Linear(16384,1024),
+            nn.Sigmoid(),
+            nn.Linear(1024, 512),
+            nn.Sigmoid(),
+            nn.Linear(512,9),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, xb):
+        return self.network(xb)
